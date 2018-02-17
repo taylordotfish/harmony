42d34e75e93febc27aba415231718195fea24731
taylor.fish <contact@taylor.fish>
2018-02-17 00:19:57 -0800

    Version 0.4.1

--- harmony/harmony.py
+++ harmony/harmony.py
@@ -33,7 +33,7 @@ import re
 import sys
 import traceback
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 RECAPTCHA_API_KEY = "6Lef5iQTAAAAAKeIvIY-DeexoO3gj7ryl9rLMEnn"
 RECAPTCHA_SITE_URL = "https://discordapp.com"
 
