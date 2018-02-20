ee908567eb53e7ad2a69457c2234c73c6bab0f90
taylor.fish <contact@taylor.fish>
2018-02-19 17:32:25 -0800

    Bump dev version

--- harmony/harmony.py
+++ harmony/harmony.py
@@ -33,7 +33,7 @@ import re
 import sys
 import traceback
 
-__version__ = "0.5.1"
+__version__ = "0.5.2-dev"
 RECAPTCHA_API_KEY = "6Lef5iQTAAAAAKeIvIY-DeexoO3gj7ryl9rLMEnn"
 RECAPTCHA_SITE_URL = "https://discordapp.com"
 
